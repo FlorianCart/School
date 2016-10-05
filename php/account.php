1290872a607f49023d8260c696515f848d05631e
radicalcs <radicalcs@gmail.com>
2016-10-05 09:18:06 -0400

    fix viewport

--- php/account.php
+++ php/account.php
@@ -4,6 +4,8 @@
 <head>
 
   <meta charset="UTF-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../css/index.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
