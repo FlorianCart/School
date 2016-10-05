8fd50b183887a33aa049ec12ea8a7db0fc547850
inglow <inglow@inglows-MacBook-Pro.local>
2016-10-05 09:42:36 +0200

    menu
    Include

--- php/quinoussommes.php
+++ php/quinoussommes.php
@@ -15,60 +15,7 @@
 
 
 
-  <nav class="navbar navbar-default navbar-static-top">
-
-    <div class="container">
-
-      <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            
-          </button>
-
-        <a class="navbar-brand" id="branding" href="main.php">SchoolTool</a>
-
-      </div>
-      <div id="navbar" class="navbar-collapse collapse">
-
-        <ul class="nav navbar-nav">
-
-          <li><a href="main.php">Accueil</a></li>
-          <li><a href="transport.php">Transport</a></li>
-          <li><a href="devoir.php">Aide aux devoirs</a></li>
-
-          <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Fonctionnement<span class="caret"></span></a>
-            <ul class="dropdown-menu">
-
-              <li class="dropdown-header">Le transport à l'école</li>
-              <li><a href="transport-how.php">Comment ça marche ?</a></li>
-              <li><a href="register.php">Devenir conducteur</a></li>
-
-              <li role="separator" class="divider"></li>
-
-              <li class="dropdown-header">Aide aux devoirs</li>
-              <li><a href="principe.php">Principes</a></li>
-              <li><a href="profs.php">Les "Professeurs"</a></li>
-              <li><a href='#'>Les élèves</a></li>
-
-            </ul>
-          </li>
-          <li class='active'><a href='quinoussommes.php'>A propos</a></li>
-        </ul>
-
-        <ul class="nav navbar-nav navbar-right hidden-sm hidden-xs">
-          <li class="active"><a href="connexion.php"><i class="fa fa-user" aria-hidden="true"></i> Mon compte</a></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right visible-sm visible-xs">
-          <li class="active"><a href="connexion.php"><i class="fa fa-user" aria-hidden="true"></i></a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php  include("menu.php") ?>
 
   <div class="container">
     <br>
