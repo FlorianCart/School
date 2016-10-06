5fc07073e4903c19656eaa0f85d0d78d521cca7c
dartook <arthurdoe77@gmail.com>
2016-10-06 11:18:37 +0200

    look at my dab, bitch

---
+++ php/ajoutercours.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" href="../css/index.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Hind|Roboto" rel="stylesheet">
+  
+  <title>Proposer un cours - SchoolTool</title>
+</head>
+
+<body>
+
+ <?php  include("menu.php") ?>
+
+ 
+ 
+
+
+
+</body>
+
+</html>
+
+
+<!-- Scripts -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+<script src="../parallax/parallax.js"></script>
